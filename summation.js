7efec170a0fe44ec3baa7eab673a84560bab1cd7
Shohail Aamil <shohail@corzent.com>
2022-06-11 08:03:51 +0530

    this is 3 commit

--- summation.js
+++ summation.js
@@ -8,3 +8,4 @@ console.log(a * b);
 
 console.log("this is first commit of this test-sqush branch");
 console.log("this is second commit of this test-sqush branch");
+console.log("this is third commit of this test-sqush branch");
